10d0522c2437980eef36bfbb222c1de5225fff4d
Verónica Monzón <veronikmonzon@gmail.com>
2020-11-13 22:25:43 -0300

    php

---
+++ Obligatorios/PHP/holaMundo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola Mundo</title>
+</head>
+<body>
+<h1 style="text-align: center; text-decoration: underline;">
+    <?php echo 'Hola Mundo!!'?>
+</h1>
+  
+</body>
+</html>
